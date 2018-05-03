c1a0113d4d0ccf9113d7b83de3b7a62fae8ef6a6
Chanan Braunstein <cbraunstein@rosettastone.com>
2018-05-03 12:19:51 -0600

    Add project files.

---
+++ src/BlazorLogger/Logger.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Interop;
+using System.Collections.Generic;
+
+namespace BlazorLogger
+{
+    public class Logger
+    {
+        public static void Clear()
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Clear");
+        }
+
+
+        //Currently does not work: https://github.com/aspnet/Blazor/issues/739
+        /*public static void Count()
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Count", null);
+        }*/
+
+        public static void Count(string label)
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Count", label);
+        }
+        public static void Error(params object[] list)
+        {
+            var _list = new List<object>(list);
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Error", _list);
+        }
+        public static void Group(string label)
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Group", label);
+        }
+        public static void GroupCollapsed(string label)
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.GroupCollapsed", label);
+        }
+        public static void GroupEnd()
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.GroupEnd");
+        }
+        public static void Info(params object[] list)
+        {
+            var _list = new List<object>(list); //This line is needed see: https://github.com/aspnet/Blazor/issues/740
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Info", _list);
+        }
+        public static void Log(params object[] list)
+        {
+            var _list = new List<object>(list); //This line is needed see: https://github.com/aspnet/Blazor/issues/740
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Log", _list);
+        }
+        public static void Table(object obj)
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Table", obj);
+        }
+        public static void Table(object obj, string[] columns)
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.TableColumns", obj, columns);
+        }
+        public static void Time(string label)
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Time", label);
+        }
+        public static void TimeEnd(string label)
+        {
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.TimeEnd", label);
+        }
+        public static void Warn(params object[] list)
+        {
+            var _list = new List<object>(list); //This line is needed see: https://github.com/aspnet/Blazor/issues/740
+            RegisteredFunction.Invoke<bool>("BlazorLogger.Logger.Warn", _list);
+        }
+    }
+}
---
+++ src/BlazorLogger/content/loggerJsInterop.js
@@ -0,0 +1,83 @@
+﻿Blazor.registerFunction('BlazorLogger.Logger.Clear', function () {
+    console.clear();
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.Count', function (label) {
+    if (label !== null) console.count(label);
+    else console.count();
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.Error', function (args) {
+    if (Array.isArray(args)) {
+        console["error"].apply(this, args)
+    } else {
+        console.error(args);
+    }
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.Group', function (label) {
+    if (label !== null) console.group(label);
+    else console.group();
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.GroupCollapsed', function (label) {
+    if (label !== null) console.groupCollapsed(label);
+    else console.groupCollapsed();
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.GroupEnd', function () {
+    console.groupEnd();
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.Info', function (args) {
+    if (Array.isArray(args)) {
+        console["info"].apply(this, args)
+    } else {
+        console.info(args);
+    }
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.Log', function (args) {
+    if (Array.isArray(args)) {
+        console["log"].apply(this, args)
+    } else {
+        console.log(args);
+    }
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.Table', function (obj) {
+    console.table(obj);
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.TableColumns', function (obj, columns) {
+    console.table(obj, columns);
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.Time', function (label) {
+    console.time(label);
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.TimeEnd', function (label) {
+    console.timeEnd(label);
+    return true;
+});
+
+Blazor.registerFunction('BlazorLogger.Logger.Warn', function (args) {
+    if (Array.isArray(args)) {
+        console["warn"].apply(this, args)
+    } else {
+        console.warn(args);
+    }
+    return true;
+});
